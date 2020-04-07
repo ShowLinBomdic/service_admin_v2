af0e3929dcf19585d5cdcb878e8e8059085d3627
kevin tu <kevin.tu@bomdic.com>
2020-04-07 14:53:12 +0800

    login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,23 @@
 
                 <form action="{{ route('login') }}" method="post">
                     @csrf
+
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
                     </div>
+
+                    <span class="invalid-feedback" role="alert">
+                        <strong>aaa</strong>
+                    </span>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -51,7 +58,8 @@
                     <div class="row">
                         <div class="col-8">
                             <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
                                 <label for="remember">
                                     Remember Me
                                 </label>
